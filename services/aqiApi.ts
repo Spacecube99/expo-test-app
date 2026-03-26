40647cbf750296ba217679a4f8284c2f683e2346
yaasir <yari7856@gmail.com>
2026-03-26 13:44:50 -0500

    Removed uneccesary Air Quality Index API service aqiAPI.ts from previous attempt to use API

--- services/aqiApi.ts
+++
@@ -1,9 +0,0 @@
-export const getAQI = async () => {
-  const url =
-    "https://air-quality-api.open-meteo.com/v1/air-quality?latitude=40.7128&longitude=-96.9906&current=us_aqi,pm10,pm2_5";
-
-  const res = await fetch(url);
-  const data = await res.json();
-
-  return data;
-};
